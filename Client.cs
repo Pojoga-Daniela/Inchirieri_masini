9fdabfb9dd725ff1adf7f220e3da4696222855b3
Pojoga-Daniela <dumitrita.pojoga@student.usv.ro>
2026-03-11 21:08:22 +0200

    Add files via upload

---
+++ Client.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace InchirieriMasini
+{
+
+    public class Client
+    {
+        public string Nume { get; set; }
+        public string Prenume { get; set; }
+        public string CNP { get; set; }
+
+        public Client(string nume, string prenume, string cnp)
+        {
+            Nume = nume;
+            Prenume = prenume;
+            CNP = cnp;
+        }
+
+        public string Info()
+        {
+            return Nume + " " + Prenume + " - CNP: " + CNP;
+        }
+    }
+}
